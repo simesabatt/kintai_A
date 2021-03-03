e060bde67a98a9d915ab50864b02fced1c5a202d
simesabatt <t95203631@gmail.com>
2021-03-04 01:05:03 +0900

    Finish edit-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,27 +70,27 @@ class UsersController < ApplicationController
 
     # beforeフィルター
 
-    # paramsハッシュからユーザーを取得します。
-    def set_user
-      @user = User.find(params[:id])
-    end
+    # # paramsハッシュからユーザーを取得します。
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
 
-    # ログイン済みのユーザーか確認します。
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "ログインしてください。"
-        redirect_to login_url
-      end
-    end
+    # # ログイン済みのユーザーか確認します。
+    # def logged_in_user
+    #   unless logged_in?
+    #     store_location
+    #     flash[:danger] = "ログインしてください。"
+    #     redirect_to login_url
+    #   end
+    # end
 
-    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
-    def correct_user
-      redirect_to(root_url) unless current_user?(@user)
-    end
+    # # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    # def correct_user
+    #   redirect_to(root_url) unless current_user?(@user)
+    # end
 
-    # システム管理権限所有かどうか判定します。
-    def admin_user
-      redirect_to root_url unless current_user.admin?
-    end
+    # # システム管理権限所有かどうか判定します。
+    # def admin_user
+    #   redirect_to root_url unless current_user.admin?
+    # end
 end
