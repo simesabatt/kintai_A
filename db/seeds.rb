377a992360eb9ec642ce7bbd5a694bf7d7ae63d1
simesabatt <t95203631@gmail.com>
2021-03-07 20:31:48 +0900

    kintai B mitame

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,29 @@
 # coding: utf-8
 
-User.create!(name: "Sample User",
+User.create!(name: "管理者",
   email: "sample@email.com",
   password: "password",
   password_confirmation: "password",
-  admin: true)
+  admin: true,
+  superior: false)
+
+User.create!(name: "上長1",
+  email: "sample1@email.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: false,
+  superior: true)
+
+User.create!(name: "上長2",
+  email: "sample2@email.com",
+  password: "password",
+  password_confirmation: "password",
+  admin: false,
+  superior: true)
 
 60.times do |n|
 name  = Faker::Name.name
-email = "sample-#{n+1}@email.com"
+email = "sample-#{n+3}@email.com"
 password = "password"
 User.create!(name: name,
     email: email,
