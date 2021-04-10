37d574acc7ef68dafe6302f56f10d89e10ccccdc
simesabatt <t95203631@gmail.com>
2021-04-10 22:42:47 +0900

    users model rename finish

--- db/migrate/20210410131236_rename_department_column_to_users.rb
+++ db/migrate/20210410131236_rename_department_column_to_users.rb
@@ -1,5 +1,8 @@
 class RenameDepartmentColumnToUsers < ActiveRecord::Migration[5.1]
   def change
     rename_column :users, :department, :affiliation
+    rename_column :users, :basic_time, :basic_work_time
+    rename_column :users, :start_basic_time, :designates_work_start_time
+    rename_column :users, :finish_basic_time, :designates_work_end_time
   end
 end
