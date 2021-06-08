facef7c09359f0983454187a5d69736eca49a60e
simesabatt <t95203631@gmail.com>
2021-06-08 23:48:46 +0900

    debug 3,6

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@ User.create!(name: "管理者",
   password_confirmation: "password",
   admin: true,
   superior: false,
-  work_now: false)
+  work_now: false,
+  employee_number: 0)
 
 User.create!(name: "上長1",
   email: "sample1@email.com",
@@ -14,7 +15,8 @@ User.create!(name: "上長1",
   password_confirmation: "password",
   admin: false,
   superior: true,
-  work_now: false)
+  work_now: false,
+  employee_number: 1)
   
 User.create!(name: "上長2",
   email: "sample2@email.com",
@@ -22,15 +24,18 @@ User.create!(name: "上長2",
   password_confirmation: "password",
   admin: false,
   superior: true,
-  work_now: false)
+  work_now: false,
+  employee_number: 2)
 
 5.times do |n|
-name  = Faker::Name.name
-email = "sample#{n+3}@email.com"
-password = "password"
-User.create!(name: name,
-    email: email,
-    password: password,
-    password_confirmation: password,
-    work_now: false)
+  name  = Faker::Name.name
+  email = "sample#{n+3}@email.com"
+  password = "password"
+  employee_number = "#{n+3}"
+  User.create!(name: name,
+      email: email,
+      password: password,
+      password_confirmation: password,
+      work_now: false,
+      employee_number: employee_number)
 end
